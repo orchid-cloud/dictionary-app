e001706f0e294aa61e8903bd871e142d5f655b74
NZ <n.prokopyk@gmail.com>
2022-10-26 16:24:47 +0200

    a component Dictionary with a search added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <header></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>Coded by</footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
