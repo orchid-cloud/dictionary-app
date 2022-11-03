08f9eb9496faaeb8340bf44b5d58e65d13613647
NZ <n.prokopyk@gmail.com>
2022-11-03 13:40:51 +0100

    component synonyms added

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -8,10 +9,18 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition:</strong>
               {definition.definition}
               <br />
-              <em>{definition.example}</em>
+              {definition.example && (
+                <>
+                  <strong>Example:</strong>
+                  <em>{definition.example}</em>
+                </>
+              )}
+              <br />
             </p>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
